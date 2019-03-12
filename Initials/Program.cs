16f633a3939d95b3bca9018d44ff43bc454cad77
Puja Grover <puja_grover@yahoo.com>
2019-03-12 12:37:58 -0500

    Week7HwInitials;Tested;Commit;Sync;Push

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -6,9 +6,54 @@ namespace Initials
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your full name:");
+            string fullName = Console.ReadLine().Trim();
+
+            string initials = Initials(fullName);
+            Console.Write($"Your Initials are: {initials}");
             Console.ReadLine();
         }
 
-        // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        public static string Initials(string fullName)
+        {
+            //Split the string into an array
+            string[] fullNameArray = fullName.Trim().ToUpper().Split(" ");
+            string userInitials = "";
+            for (int i = 0; i < fullNameArray.Length; i++)
+            {
+                string firstNameInitial = fullNameArray[i];
+                userInitials += firstNameInitial[0];
+
+            }
+            return userInitials;
+
+        }
+        //public static string InitialsOfFullName(string userFullName)
+        //{
+
+        //    //Convert User input to an array of words
+        //    // string[] userInputWordListArray = userInputWordList.ToLower().Split(",");
+
+        //   // string[] userFullNameArray = userFullName.ToUpper().Split(" ");
+        //    char[] userFullNameArray = userFullName.ToCharArray();
+        //    char firstNameInitial;
+        //    char lastNameInitial;
+
+        //    firstNameInitial = userFullNameArray[0];
+        //    Console.Write(firstNameInitial);
+        //    //Console.ReadLine();
+
+        //    for (int i= 0; i < userFullNameArray.Length; i++)
+        //    {
+        //        if (userFullNameArray[i] == ' ')
+        //        {
+        //            lastNameInitial = userFullNameArray[i + 1];
+        //            Console.WriteLine(lastNameInitial);
+        //            //Console.ReadLine();
+        //        }
+
+        //    }
+        //    return "";
+        //}
     }
 }
